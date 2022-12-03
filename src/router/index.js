4f0d0ef779b5f0bc240709ab8f6d007b0a3d2ae0
zq0079 <009308ms@163.com>
2022-12-04 03:52:25 +0800

    添加BOOK

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Book from '@/components/Book'
 Vue.use(Router)
 
 export default new Router({
