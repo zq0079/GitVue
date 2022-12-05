c1aaee70163dcab231dcaf47691efb7d6adf4b6d
zq0079 <009308ms@163.com>
2022-12-06 07:23:44 +0800

    20221206001

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
 import Book from '@/components/Book.vue'
+import pageOne from '@/components/pageOne.vue'
 
 export default new VueRouter({
   routes: [
@@ -11,6 +12,10 @@ export default new VueRouter({
     {
       path: '/Book',
       component: Book
+    },
+    {
+      path: '/pageOne',
+      component: pageOne
     }
   ]
 })
